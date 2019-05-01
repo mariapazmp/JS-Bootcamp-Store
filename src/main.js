9831a54a05e2eb78b52125120b3c1882f6ca216c
Pazu <libelulaby@gmail.com>
2019-04-30 23:09:58 -0500

    First Commit. Code Base and Footer

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+//Create Catalog object
+
+const catalog = {
+    001: "Women",
+    002: "Men",
+    003: "Electronics",
+    004: "Footwear",
+    005: "Accesories",
+    006: "Suits",
+    007: "Jackets"
+}
